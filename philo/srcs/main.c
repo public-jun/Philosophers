aea5eb4ab03756c2f0074fa3dfa019f153987bab
public-jun <generalpublicjunjun@gmail.com>
2021-08-27 16:32:55 +0900

    norm

--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -6,13 +6,13 @@
 /*   By: jnakahod <jnakahod@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/26 17:48:35 by jnakahod          #+#    #+#             */
-/*   Updated: 2021/08/27 14:22:39 by jnakahod         ###   ########.fr       */
+/*   Updated: 2021/08/27 16:29:58 by jnakahod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <philo.h>
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
 	t_info	info;
 
