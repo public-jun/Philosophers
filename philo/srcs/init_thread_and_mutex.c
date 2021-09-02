8e60ee542efa6ca6b2c2749028a4f54de948194f
public-jun <generalpublicjunjun@gmail.com>
2021-09-02 21:52:20 +0900

    add init_thread_and_mutex

--- philo/srcs/init_thread_and_mutex.c
+++ philo/srcs/init_thread_and_mutex.c
@@ -6,13 +6,83 @@
 /*   By: jnakahod <jnakahod@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/27 22:26:25 by jnakahod          #+#    #+#             */
-/*   Updated: 2021/08/27 23:24:35 by jnakahod         ###   ########.fr       */
+/*   Updated: 2021/09/02 21:35:51 by jnakahod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <philo.h>
 
-int init_thread_and_mutex(t_config *config)
+int	init_share(t_share *share)
 {
+	share->is_stop = false;
+	if (pthread_mutex_init(&(share->mutex_print), NULL) !=0)
+		return (-1);
+	if (pthread_mutex_init(&(share->mutex_stop), NULL) != 0)
+	{
+		pthread_mutex_destroy(&(share->mutex_print));
+		return (-1);
+	}
+	return (0);
+}
+
+int	init_philo(t_trunk *trunk, int num_philos)
+{
+	int i;
+
+	trunk->philo = (t_philo *)malloc(sizeof(t_philo) * num_philos);
+	if (!trunk->philo)
+		return (-1);
+	i = 0;
+	while (i < num_philos)
+	{
+		trunk->philo[i].eat_count = 0;
+		trunk->philo[i].left = NULL;
+		trunk->philo[i].right = NULL;
+		i++;
+	}
+	return (0);
+}
+
+int	init_fork(t_trunk *trunk, int num_forks)
+{
+	int		i;
+	t_fork	*tmp_fork;
+
+	trunk->fork = (t_fork *)malloc(sizeof(t_fork) * num_forks);
+	if (!trunk->fork)
+		return (-1);
+	i = 0;
+	tmp_fork = &(trunk->fork[i]);
+	while (i < num_forks)
+	{
+		tmp_fork->is_use = false;
+		if (pthread_mutex_init(&(tmp_fork->mutex_fork), NULL) != 0)
+		{
+			//エラー処理 free (trunk->philo, trunk->fork)
+			return (-1);
+		}
+		tmp_fork = &(trunk->fork[++i]);
+	}
+	return (0);
+}
+
+int init_thread_and_mutex(t_trunk *trunk)
+{
+	int num_philo_and_fork;
+
 	// thread を作る
+	num_philo_and_fork = trunk->config.num_philo_and_fork;
+	if (init_share(&(trunk->share)) < 0)
+		return (-1);
+	if (init_philo(trunk, num_philo_and_fork) < 0)
+		return (-1);
+	if (init_fork(trunk, num_philo_and_fork) < 0)
+		return (-1);
+	printf("philo[num]\t| eat_count | left | right\n");
+	for (int i = 0; i < num_philo_and_fork; i++)
+	{
+		printf("philo[%d]\t|\t%d\t|\n", i + 1, trunk->philo[i].eat_count);
+	}
+	printf()
+	return (0);
 }
