103c335bac0f9b30ad96ab720aef9f3753fe771f
public-jun <generalpublicjunjun@gmail.com>
2021-09-06 22:29:16 +0900

    update test

--- demo/test.c
+++ demo/test.c
@@ -6,61 +6,46 @@
 /*   By: jnakahod <jnakahod@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/19 21:19:52 by jnakahod          #+#    #+#             */
-/*   Updated: 2021/08/19 22:31:01 by jnakahod         ###   ########.fr       */
+/*   Updated: 2021/09/06 10:25:01 by jnakahod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <pthread.h>
 
-int	a = 0;
-pthread_mutex_t	mutex;
+int a = 0;
+pthread_mutex_t mutex;
 
-void	*func_thread(void *p)
+void *func_thread1(void *p)
 {
 	printf("start %d\n", *(int *)p);
 
-	int i = 0;
-	for (i = 0; i < 10000; i++)
-	{
-		pthread_mutex_lock(&mutex);
-		int next = a + 1;
-		int now = a;
-		a = next;
-		pthread_mutex_unlock(&mutex);
-		if (now + 1 != next)
-		{
-			printf("other thread change %d %d\n", a+1, next);
-		}
-	}
+	pthread_mutex_lock(&mutex);
+	while (1);
+	pthread_mutex_unlock(&mutex);
 	return (0);
 }
 
-int	main(void)
+void *func_thread2(void *p)
+{
+	pthread_mutex_lock(&mutex);
+	printf("aaa\n");
+	pthread_mutex_unlock(&mutex);
+	return (0);
+}
+
+int main(void)
 {
 	pthread_mutex_init(&mutex, NULL);
 	printf("test\n");
 
-	int	b = 42;
+	int b = 42;
 
-	pthread_t	pthread;
-	pthread_create(&pthread, NULL, &func_thread, &b);
-
-	int i = 0;
-	for (i = 0; i < 10000; i++)
-	{
-		pthread_mutex_lock(&mutex);
-		int next = a + 1;
-		int	now = a;
-		a = next;
-		pthread_mutex_unlock(&mutex);
-		if (now + 1 != next)
-		{
-			printf("other thread change %d %d\n", a + 1, next);
-		}
-	}
-	pthread_join(pthread, NULL);
-	printf("a=%d\n", a);
+	pthread_t pthread1, pthread2;
+	pthread_create(&pthread1, NULL, &func_thread1, &b);
+	pthread_create(&pthread2, NULL, &func_thread2, &b);
+	pthread_join(pthread1, NULL);
+	pthread_join(pthread2, NULL);
 
 	return (0);
 }
