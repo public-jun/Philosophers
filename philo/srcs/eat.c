66e13c5d04c48fd41e6108cd39c2efe04ea4dddc
public-jun <generalpublicjunjun@gmail.com>
2021-09-10 18:00:13 +0900

    Revert "修正案"
    This reverts commit b9764f3cd487c329189f7fdb74c09b5073826bf5.
    
    戻す

--- philo/srcs/eat.c
+++ philo/srcs/eat.c
@@ -6,7 +6,7 @@
 /*   By: jnakahod <jnakahod@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/07 14:59:07 by jnakahod          #+#    #+#             */
-/*   Updated: 2021/09/10 13:53:34 by jnakahod         ###   ########.fr       */
+/*   Updated: 2021/09/10 00:44:13 by jnakahod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,49 +22,45 @@ int eating(t_philo *philo)
 		pthread_mutex_unlock(&g_print);
 	}
 	else
-	{
-		pthread_mutex_unlock(&(philo->left->mutex_fork));
-		pthread_mutex_unlock(&(philo->right->mutex_fork));
 		return (-1);
-	}
 	while (!waiting_time(philo->eat_start, philo->config.time_to_eat))
 	{
 		if (g_is_dead == true)
-		{
-			pthread_mutex_unlock(&(philo->left->mutex_fork));
-			pthread_mutex_unlock(&(philo->right->mutex_fork));
 			return (-1);
-		}
-		usleep(50);
+		usleep(200);
 	}
 	philo->eat_count++;
-	pthread_mutex_unlock(&(philo->left->mutex_fork));
-	pthread_mutex_unlock(&(philo->right->mutex_fork));
+	philo->right->is_use = false;
+	philo->left->is_use = false;
 	return (0);
 }
 
 int take_a_fork(t_fork *hand, t_philo *philo)
 {
-	pthread_mutex_lock(&(hand->mutex_fork));
-	if (g_is_dead == false)
+	if (hand->is_use == false)
 	{
-		pthread_mutex_lock(&g_print);
-		printf("%ld %d has taken a fork\n", what_time(), philo->id);
-		pthread_mutex_unlock(&g_print);
+		pthread_mutex_lock(&(hand->mutex_fork));
+		hand->is_use = true;
+		if (g_is_dead == false)
+		{
+			pthread_mutex_lock(&g_print);
+			printf("%ld %d has taken a fork\n", what_time(), philo->id);
+			pthread_mutex_unlock(&g_print);
+		}
+		pthread_mutex_unlock(&(hand->mutex_fork));
+		return (1);
 	}
-	// pthread_mutex_unlock(&(hand->mutex_fork));
-	return (1);
+	return (0);
 }
 
 int wait_fork(t_fork *hand, t_philo *philo)
 {
-	// while (!take_a_fork(hand, philo))
-	// {
-	// 	if (g_is_dead == true)
-	// 		return (-1);
-	// 	usleep(50);
-	// }
-	take_a_fork(hand, philo);
+	while (!take_a_fork(hand, philo))
+	{
+		if (g_is_dead == true)
+			return (-1);
+		usleep(200);
+	}
 	return (0);
 }
 
