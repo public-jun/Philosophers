cd23ef7fe9acfa97875f725f0f0c1fc807d74ef8
public-jun <generalpublicjunjun@gmail.com>
2021-11-27 18:20:29 +0900

    Add: パラメータの有効範囲を判定

--- philo/srcs/init_args.c
+++ philo/srcs/init_args.c
@@ -6,7 +6,7 @@
 /*   By: jnakahod <jnakahod@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/27 16:54:33 by jnakahod          #+#    #+#             */
-/*   Updated: 2021/11/27 18:03:21 by jnakahod         ###   ########.fr       */
+/*   Updated: 2021/11/27 18:14:44 by jnakahod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,28 @@ static t_result	set_philo_para(int *item, char *arg)
 	return (SUCCESS);
 }
 
+bool	is_in_range(int value, int min, int max)
+{
+	if (min <= value && value <= max)
+		return (true);
+	else
+		return (false);
+}
+
+t_result	check_valid_philo_para(t_philo *philo)
+{
+	if (!is_in_range(philo->num_philo_and_fork, PHILOMIN, PHILOMAX)
+		|| !is_in_range(philo->time_to_die, MSECMIN, MSECMAX)
+		|| !is_in_range(philo->time_to_eat, MSECMIN, MSECMAX)
+		|| !is_in_range(philo->time_to_sleep, MSECMIN, MSECMAX))
+		return (FAILURE);
+	if (philo->is_must_eat
+		&& !is_in_range(philo->num_of_times_each_philo_must_eat,
+			MUSTEATMIN, MUSTEATMAX))
+		return (FAILURE);
+	return (SUCCESS);
+}
+
 t_result	init_args(int ac, char **av, t_philo *philo)
 {
 	if (ac != 5 && ac != 6)
@@ -38,6 +60,8 @@ t_result	init_args(int ac, char **av, t_philo *philo)
 		return (FAILURE);
 	if (philo->is_must_eat
 		&& set_philo_para(&philo->num_of_times_each_philo_must_eat, av[5]))
+		return (FAILURE);
+	if (check_valid_philo_para(philo))
 		return (FAILURE);
 	return (SUCCESS);
 }
