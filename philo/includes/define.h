874f52e6bb061b290f18502ab799b1c98cbf993c
public-jun <generalpublicjunjun@gmail.com>
2021-11-29 18:42:07 +0900

    Add: least philo ate count

--- philo/includes/define.h
+++ philo/includes/define.h
@@ -6,7 +6,7 @@
 /*   By: jnakahod <jnakahod@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/27 15:14:46 by jnakahod          #+#    #+#             */
-/*   Updated: 2021/11/29 11:39:30 by jnakahod         ###   ########.fr       */
+/*   Updated: 2021/11/29 16:12:09 by jnakahod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,8 @@
 # define ERRINITMUTEX "Cant init mutex"
 # define TAKEFORK "has taken a fork"
 # define EAT "is eating"
+# define SLEEP "is sleeping"
+# define THINK "is thinking"
 # define DIE "died"
 
 #endif
