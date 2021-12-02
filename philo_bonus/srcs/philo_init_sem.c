f703d3f8dd57d4750acaffd86f7b3417817036c3
public-jun <generalpublicjunjun@gmail.com>
2021-12-02 14:48:37 +0900

    Add: err philo_init_sem

--- philo_bonus/srcs/philo_init_sem.c
+++ philo_bonus/srcs/philo_init_sem.c
@@ -6,7 +6,7 @@
 /*   By: jnakahod <jnakahod@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/27 23:00:59 by jnakahod          #+#    #+#             */
-/*   Updated: 2021/12/02 14:36:05 by jnakahod         ###   ########.fr       */
+/*   Updated: 2021/12/02 14:46:57 by jnakahod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,29 @@ t_result	new_semaphore(sem_t **dst, const char *name, int value)
 {
 	sem_unlink(name);
 	*dst = sem_open(name, O_CREAT, 0600, value);
+	if (*dst == SEM_FAILED)
+		return (FAILURE);
 	sem_unlink(name);
 	return (SUCCESS);
 }
 
+void	close_philo_sem(t_philo *philo)
+{
+	if (philo->fork)
+		sem_close(philo->fork);
+	if (philo->died)
+		sem_close(philo->died);
+	if (philo->eat)
+		sem_close(philo->eat);
+}
 
+t_result	err_philo_init_sem(t_philo *philo)
+{
+	close_philo_sem(philo);
+	free_all(philo);
+	ft_err(ERRSEM);
+	return (FAILURE);
+}
 
 t_result	philo_init_sem(t_philo *philo)
 {
@@ -30,10 +48,11 @@ t_result	philo_init_sem(t_philo *philo)
 
 	total_philo = philo->num_philo_and_fork;
 	taking = NULL;
-	new_semaphore(&philo->fork, "/fork", total_philo);
-	new_semaphore(&philo->eat, "/eat", 0);
-	new_semaphore(&philo->died, "/died", 1);
-	new_semaphore(&taking, "/taking", 1);
+	if (new_semaphore(&philo->fork, "/fork", total_philo)
+		|| new_semaphore(&philo->eat, "/eat", 0)
+		|| new_semaphore(&philo->died, "/died", 1)
+		|| new_semaphore(&taking, "/taking", 1))
+			return (err_philo_init_sem(philo));
 	i = -1;
 	while (++i < total_philo)
 	{
