d68a3996e01dd93d3c969b2cda6652082a681e09
public-jun <generalpublicjunjun@gmail.com>
2021-12-08 19:08:24 +0900

    Fix: only one philo has thread

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: jnakahod <jnakahod@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/27 15:35:10 by jnakahod          #+#    #+#             */
-/*   Updated: 2021/12/03 22:47:45 by jnakahod         ###   ########.fr       */
+/*   Updated: 2021/12/08 19:05:41 by jnakahod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -113,7 +113,7 @@ void		philo_lunch(t_philo *philo);
 ** philo_utils.c
 */
 void		philo_print_status(t_man *man, const char *msg);
-void		only_one_philo(t_man *man);
+void		*only_one_philo(void *p);
 void		philo_wait(t_man *man, long long standard, int wait_time);
 void		philo_die(t_man *man);
 
--- philo/srcs/philo_utils.c
+++ philo/srcs/philo_utils.c
@@ -6,7 +6,7 @@
 /*   By: jnakahod <jnakahod@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/29 19:55:45 by jnakahod          #+#    #+#             */
-/*   Updated: 2021/11/29 20:00:58 by jnakahod         ###   ########.fr       */
+/*   Updated: 2021/12/08 19:07:44 by jnakahod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,15 @@ void	philo_print_status(t_man *man, const char *msg)
 	pthread_mutex_unlock(man->died);
 }
 
-void	only_one_philo(t_man *man)
+void	*only_one_philo(void *p)
 {
+	t_man	*man;
+
+	man = (t_man *)p;
 	philo_print_status(man, TAKEFORK);
 	man->time_to_start_eat = what_time();
 	philo_wait(man, man->time_to_start_eat, man->time_to_die);
+	return (NULL);
 }
 
 void	philo_wait(t_man *man, long long standard, int wait_time)
